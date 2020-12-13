8b718f004635ff95fd6c3d91ea3f8fe86a9a5e07
indranil2507 <iindranilsaha1@gmail.com>
2020-12-13 13:29:34 +0530

    hero

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
+import Video from '../../videos/video.mp4'
+import {HeroContainer,
+        HeroBg,
+        VideoBg} from './HeroElements'
 
 const HeroSection = () => {
     return (
         <HeroContainer>
             <HeroBg>
-                <VideoBg autoPlay loop muted arc={Video} type='video/mp4' />
+                <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
 
                 
             </HeroBg>
+            <HeroContent>
+                <HeroH1>
+                    Virtual Banking Made Easy
+                </HeroH1>
+                <HeroP>
+                    Sign up for a new account today and receive $250 in 
+                    credit towards your 
+                    next payment
+                </HeroP>
+                </HeroContent>
         </HeroContainer>
     )
 }
